11b53332894bf870829ef62498104949bfc327f5
[Kirubha Shankar A] <[23cs103@skcet.ac.in]>
2024-10-28 16:10:00 +0530

    completed whole app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Front from "./front";
+import LoginPage from "./login";
+import Register from "./register";
+import ProductGrid from "./smartphones";
+import Home from "./home";
+import Offers from "./offers";
+import About from "./contact";
+import Orders from "./orders";
+import Cart from "./cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Front />} />
+          <Route path='/register' element={ <Register />} />
+          <Route path='/login' element={ <LoginPage />} />
+          <Route path='/home' element={ <Home />} />
+          <Route path='/offers' element={ <Offers />} />
+          <Route path='/contact' element={ <About />} />
+          <Route path='/orders' element={ <Orders />} />
+          <Route path='/cart' element={ <Cart />} />
+          
+        </Routes>
+      </Router>
+    
+      
   );
 }
 
